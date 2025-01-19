708d25fab4fc1e47c4d27e60b170756d0483ce4d
Amarachi <amyewulonu@gmail.com>
2025-01-19 08:27:31 +0000

    exercises/java-collections/src/main/java/com/cbfacademy/CollectionsExercises.java

--- exercises/java-collections/src/main/java/com/cbfacademy/CollectionsExercises.java
+++ exercises/java-collections/src/main/java/com/cbfacademy/CollectionsExercises.java
@@ -19,76 +19,82 @@ public class CollectionsExercises {
         list.addLast(2);
         //  - add 4 as the 3rd element of the list
         list.add(2, 4);
-        //  - invoke the method element() on the list and print the result on the screen
-        System.out.println("Fisrt element using element(): " + list.element());
+         // Print the first element using element()
+         System.out.print(list.element());
         //  - return the list
         return list;
         }
 
     public Stack<Integer> useStack() {
-        // TODO: create an empty stack
+        // Create an empty stack
         Stack<Integer> stack = new Stack<>();
         //  - add 5, 6, 8, 9 to the stack
         stack.push(5);
         stack.push(6);
         stack.push(8);
         stack.push(9);
-        //  - print the first element of the stack on the screen
-        System.out.println("Top element of the stack: " + stack.peek());
-        //  - print the last element of the stack on the screen
-        System.out.println("bottom element of the stack: " + stack.firstElement());
-        //  - invoke the method pop() on the stack and print the result on the screen
-        System.out.println("Popped element: " + stack.pop());
-        //  - invoke the push(4) method on the stack
-        stack.push(4);
-        //  - return the stack
-        return stack;
+
+       // Print the first element (bottom) and the last element (top)
+       System.out.print(stack.firstElement()); // Bottom element
+       System.out.print(stack.peek());         // Top element
+
+       // Pop the top element and print it
+       System.out.print(stack.pop());
+
+       // Push 4 onto the stack
+       stack.push(4);
+
+       // Return the stack
+       return stack;
         }
 
     public ArrayDeque<Integer> useArrayDeque() {
-        // Create an empty arrayDeque
-        ArrayDeque<Integer> queue = new ArrayDeque<>();
-        //  - add 5, 6, 8, 9 to the queue
-        queue.add(5);
-        queue.add(6);
-        queue.add(8);
-        queue.add(9);
-        //  - print the first element of the queue on the screen
-        System.out.println("First element in the queue: " + queue.peekFirst());
-        //  - print the last element of the queue on the screen
-        System.out.print("Last element in the queue: " + queue.peekLast());
-        //  - invoke the method poll() on the queue and print the result on the screen
-        System.out.println("polled element: " + queue.poll());
-        //  - invoke the element() method on the queue and print the result on the screen
-        System.out.println("First element using element(): " + queue.element ());
-        //  - return the queue
-        return queue;
-        }
+        // Create an empty ArrayDeque
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
 
+        // Add 5, 6, 8, 9 to the deque
+        deque.add(5);
+        deque.add(6);
+        deque.add(8);
+        deque.add(9);
+
+        // Print the first and last elements
+        System.out.print(deque.peekFirst()); // First element
+        System.out.print(deque.peekLast());  // Last element
+
+        // Poll the first element and print it
+        System.out.print(deque.poll());
+
+        // Get the first element using element() and print it
+        System.out.print(deque.element());
+
+        // Return the deque
+        return deque;
+    }
+
+  
     public HashMap<Integer, String> useHashMap() {
         // Create an empty hash map
         HashMap<Integer, String> map = new HashMap<>();
-        //  - add {1, TypeScript} entry to the map
-        map.put(1,"typeScript");
-        //  - add {2, Kotlin} entry to the map
+
+        // Add entries to the map
+        map.put(1, "TypeScript");
         map.put(2, "Kotlin");
-        //  - add {3, Python} entry to the map
         map.put(3, "Python");
-        //  - add {4, Java} entry to the map
         map.put(4, "Java");
-        //  - add {5, JavaScript} entry to the map
         map.put(5, "JavaScript");
-        //  - add {6, Rust} entry to the map
         map.put(6, "Rust");
-        //  - determine the set of keys from the map and print it on the screen
-        System.out.println("Keys in the map: " + map.keySet());
-        //  - determine the set of values from the map and print it on the screen
-        System.out.println("Values in the map: " + map.values());
-        //  - determine whether the map contains "English" as a language and print the result on the screen
-        System.out.println("Map contains 'English': " + map.containsValue("English"));
-        //  - return the map
+
+        // Print the set of keys
+        System.out.print(map.keySet());
+
+        // Determine if the map contains "English" and print the result
+        System.out.print(map.containsValue("English"));
+
+        // Return the map
         return map;
-        }
+    }
+
 
     public String getName() {
         return "Collections Exercises";
